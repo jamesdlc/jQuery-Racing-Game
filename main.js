710d845813371a30b01d786b3abc8a84feed5c90
jamestothe <jamesdelacruz@gmail.com>
2016-07-09 16:07:15 -0700

    added player 2 function

--- main.js
+++ main.js
@@ -1,10 +1,7 @@
 console.log("test!");
 $(document).ready(function() {
-
   player1keys();
-
-
-
+  player2keys();
 });
 
 
@@ -18,13 +15,32 @@ function player1keys(){
   $(document).keyup(function (e){
     for(i=0; i < 10; i++){
       if(counter%2 === 0){
+        if(e.which == 49){
+          $(".piece").css("left",("+=2")); //
+        }
+      }
+      else if(counter%2 === 1){
+        if(e.which == 50){
+          $(".piece").css("left",("+=2"));
+        }
+      }
+      counter++;
+    }
+  });
+}
+
+function player2keys(){
+  var counter = 0;
+  $(document).keyup(function (e){
+    for(i=0; i < 10; i++){
+      if(counter%2 === 0){
         if(e.which == 48){
-          $(".piece").css("left",("+=1")); //
+          $(".piece2").css("left",("+=2")); //
         }
       }
       else if(counter%2 === 1){
         if(e.which == 57){
-          $(".piece").css("left",("+=1"));
+          $(".piece2").css("left",("+=2"));
         }
       }
       counter++;
