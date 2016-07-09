51e95c023a5cf87a205938739112a3803c4eab06
jamestothe <jamesdelacruz@gmail.com>
2016-07-09 15:47:37 -0700

    created player 1 function will work on finish line

--- main.js
+++ main.js
@@ -1,11 +1,33 @@
 console.log("test!");
-
 $(document).ready(function() {
+
+  player1keys();
+
+
+
+});
+
+
+
+
+// $(".piece").addClass("p1motion");
+// $(".piece2").animate({width: "1200px"});
+// $(".p1motion").css("left",("10px"));
+function player1keys(){
+  var counter = 0;
   $(document).keyup(function (e){
-    if(e.which == 48){
-      // $(".piece").animate({width: "1000px"});
-      // $(".piece2").animate({width: "1200px"});
-      $(".piece").addClass("p1motion");
+    for(i=0; i < 10; i++){
+      if(counter%2 === 0){
+        if(e.which == 48){
+          $(".piece").css("left",("+=1")); //
+        }
+      }
+      else if(counter%2 === 1){
+        if(e.which == 57){
+          $(".piece").css("left",("+=1"));
+        }
+      }
+      counter++;
     }
   });
-});
+}
