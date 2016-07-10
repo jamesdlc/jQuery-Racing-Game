cd1664909d2f462b8a843dc459739952db2b380a
jamestothe <jamesdelacruz@gmail.com>
2016-07-10 02:07:45 -0700

    made some fun style adjustments and supporting jquery NEED TO CONCENTRATE ON FINISH LINE

--- main.js
+++ main.js
@@ -4,20 +4,24 @@ $(document).ready(function(){
   var counter = 0;
   player1keys();
   player2keys();
+
   counter++;
-  if (counter === 43){
-    alert("player 1 wins!");
-    return false;
-  }
+  // if (counter === 43){
+  //   alert("player 1 wins!");
+  //   return false;
+  // }
 });
 
 function player1keys(){
   $(document).keyup(function (e){
     if(e.which == 49){
-      $(".piece").css("left",("+=15px"));
+      $(".p1").css("left",("+=23px"));
     }
     if(e.which == 50){
-      $(".piece").css("left",("+=15px"));
+      $(".p1").css("left",("+=23px"));
+    }
+    if($(".p1").offset().left >= 600){
+      $(".p1").removeClass("startingpiece").addClass("addKhaledP1");
     }
   });
 }
@@ -25,10 +29,13 @@ function player1keys(){
 function player2keys(){
   $(document).keyup(function (e){
     if(e.which == 48){
-      $(".piece2").css("left",("+=15px")); //
+      $(".p2").css("left",("+=23px")); //
     }
     if(e.which == 57){
-      $(".piece2").css("left",("+=15px"));
+      $(".p2").css("left",("+=23px"));
+    }
+    if($(".p2").offset().left >= 900){
+      $(".p2").removeClass("startingpiece").addClass("winning");
     }
   });
 }
