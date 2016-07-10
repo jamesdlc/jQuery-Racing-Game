74da4ccea1753e718f70ae0233ee1988db5ad07e
jamestothe <jamesdelacruz@gmail.com>
2016-07-09 19:26:54 -0700

    figured out potential win checker will make beter

--- main.js
+++ main.js
@@ -1,48 +1,37 @@
 console.log("test!");
-$(document).ready(function() {
+
+$(document).ready(function(){
   player1keys();
   player2keys();
+
 });
 
-
-
-
-// $(".piece").addClass("p1motion");
-// $(".piece2").animate({width: "1200px"});
-// $(".p1motion").css("left",("10px"));
 function player1keys(){
   var counter = 0;
   $(document).keyup(function (e){
-    for(i=0; i < 10; i++){
-      if(counter%2 === 0){
-        if(e.which == 49){
-          $(".piece").css("left",("+=2")); //
-        }
-      }
-      else if(counter%2 === 1){
-        if(e.which == 50){
-          $(".piece").css("left",("+=2"));
-        }
-      }
+    if(e.which == 49){
+      $(".piece").css("left",("+=30px"));
       counter++;
+    }
+    if(e.which == 50){
+      $(".piece").css("left",("+=30px"));
+      counter++;
+    }
+    if (counter === 43){
+      alert("player 1 wins!");
+      return false;
     }
   });
 }
 
 function player2keys(){
-  var counter = 0;
   $(document).keyup(function (e){
-    for(i=0; i < 10; i++){
-      if(counter%2 === 0){
-        if(e.which == 48){
-          $(".piece2").css("left",("+=2")); //
-        }
-      }
-      else if(counter%2 === 1){
-        if(e.which == 57){
-          $(".piece2").css("left",("+=2"));
-        }
-      }
+    if(e.which == 48){
+      $(".piece2").css("left",("+=30px")); //
+      counter++;
+    }
+    if(e.which == 57){
+      $(".piece2").css("left",("+=30px"));
       counter++;
     }
   });
