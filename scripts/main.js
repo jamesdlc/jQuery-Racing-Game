85d0bdb70b874adf171cfae3b9e483e804faade0
jamestothe <jamesdelacruz@gmail.com>
2016-07-11 10:01:03 -0700

    added html elements js and css to display winner name

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,4 @@
 console.log("Sanity Check!");
-
 $(document).ready(function(){
   var player1 = new Player("Player 1", 49, 50, "p1");
   var player2 = new Player("Player 2", 48, 57, "p2");
@@ -8,7 +7,7 @@ $(document).ready(function(){
     $(".p1art").css("left",("0"));
   });
 });
-
+//TODO: add player input
 function Player(name, key1, key2, classSelector) {
   this.name = name;
   this.keys = [key1, key2]; //array that accepts ASCII codes
@@ -20,33 +19,34 @@ function Player(name, key1, key2, classSelector) {
 }
 
 function playerMoveListener(player1, player2){ //function will exectute Player.move
-    $(document).keyup(function (e){
-      imageChanger(player1, player2);
-      if (player1.keys.indexOf(e.which) !== -1 ){ /*checking the indexOf keys indexes that !== -1*/
-          player1.move();
-          winChecker(player1);
-      }
-      else if (player2.keys.indexOf(e.which) !== -1){
-          player2.move();
-          winChecker(player2);
-      }
-    });
+  $(document).keyup(function (e){
+    imageChanger(player1, player2);
+    if (player1.keys.indexOf(e.which) !== -1 ){ /*checking the indexOf keys indexes that !== -1*/
+      player1.move();
+      winChecker(player1);
+    }
+    else if (player2.keys.indexOf(e.which) !== -1){
+      player2.move();
+      winChecker(player2);
+    }
+  });
 }
 function winChecker(p){
-  if (p.$selector.offset().left > 600){
+  if (p.$selector.offset().left > 1400){
+    console.log(p.name + " Wins!");
     p.wins += 1;
-
-}
+    $(".showWinner").text(p.name + " wins!"); //displays winner name after crossing the finish line
+  }
 }
 function imageChanger(player1, player2){ //function that changes DJ Khaled's facial expression depending on winning or losing
-  if (player1.$selector.offset().left < player2.$selector.offset().left){
-     player1.$selector.removeClass("starting").addClass("losing");
-     player2.$selector.removeClass("losing").addClass("starting");
-   }
-  else if (player1.$selector.offset().left > player2.$selector.offset().left){
-    player2.$selector.removeClass("starting").addClass("losing");
-    player1.$selector.removeClass("losing").addClass("starting");
-  }
+if (player1.$selector.offset().left < player2.$selector.offset().left){
+  player1.$selector.removeClass("starting").addClass("losing");
+  player2.$selector.removeClass("losing").addClass("starting");
+}
+else if (player1.$selector.offset().left > player2.$selector.offset().left){
+  player2.$selector.removeClass("starting").addClass("losing");
+  player1.$selector.removeClass("losing").addClass("starting");
+}
 }
 //potential reset function
 // function stopMvmt(p) {
