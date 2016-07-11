08461de322de2099f538d882d9662e69215f5b6f
jamestothe <jamesdelacruz@gmail.com>
2016-07-10 18:39:57 -0700

    deleted excess files

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 console.log("Sanity Check!");
+var winner = false;
 $(document).ready(function(){
   var player1 = new Player("test", 49, 50, "p1");
   var player2 = new Player("test", 48, 57, "p2");
@@ -20,10 +21,11 @@ function Player(name, key1, key2, racerid) {
 function playerMoveListener(p1, p2){ //function will exectute Player.move
     $(document).keyup(function (e){
       imageChanger(p1, p2);
-      if (p1.keys.indexOf(e.which) !== -1 ){
+      if (p1.keys.indexOf(e.which) !== -1 ){ //checking for the value of e.which in the indexOf keys
+        // if doesn't equal -1 then they are playerkeys
           p1.move();
           winChecker(p1);
-      } // if doesn't equal -1 then they are player1keys
+      } 
       else if (p2.keys.indexOf(e.which) !== -1){
           p2.move();
           winChecker(p2);
@@ -32,7 +34,9 @@ function playerMoveListener(p1, p2){ //function will exectute Player.move
 }
 function winChecker(p){
   if(p.$racer.offset().left > 500){
-    console.log("here");
+    p.wins += 1;
+    console.log(p.wins);
+    winner = true;
   }
 }
 function imageChanger(p1, p2){ //function that changes DJ Khaled's facial expression depending on winning or losing
@@ -45,34 +49,3 @@ function imageChanger(p1, p2){ //function that changes DJ Khaled's facial expres
     p1.$racer.removeClass("addKhaledP1").addClass("startingpiece");
   }
 }
-//
-// function player1keys(){
-//   $(document).keyup(function (e){
-//     if(e.which == 49){
-//       $(".p1").css("left",("+=23px"));
-//     }
-//     if(e.which == 50){
-//       $(".p1").css("left",("+=23px"));
-//     }
-//     if($(".p1").offset().left >= 600){
-//       $(".p1").removeClass("startingpiece").addClass("addKhaledP1");
-//     }
-//     if($(".p1").offset().left >= 1250){
-//       alert("Player 1 Wins");
-//     }
-//   });
-// }
-//
-// function player2keys(){
-//   $(document).keyup(function (e){
-//     if(e.which == 48){
-//       $(".p2").css("left",("+=23px")); //
-//     }
-//     if(e.which == 57){
-//       $(".p2").css("left",("+=23px"));
-//     }
-//     if($(".p2").offset().left >= 900){
-//       $(".p2").removeClass("startingpiece").addClass("winning");
-//     }
-//   });
-// }
